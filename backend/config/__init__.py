09bf9fc30b3fdfa2f5765378ab2ec3ae5743a9cb
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-06-09 02:27:42 +0530

    config module

---
+++ backend/config/__init__.py
@@ -0,0 +1,21 @@
+"""
+initalised the config module
+"""
+
+import configeration as c
+
+DEV_ENV = c.DEV_ENV
+DEBUG = c.DEBUG
+BACKEND_HOST = c.BACKEND_HOST
+BACKEND_PORT = c.BACKEND_PORT
+logger = c.logger
+get_logger = c.get_logger
+
+__all__ = [
+    "DEV_ENV",
+    "DEBUG",
+    "BACKEND_HOST",
+    "BACKEND_PORT",
+    "logger",
+    "get_logger",
+]
