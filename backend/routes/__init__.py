81e1fd3778dc119402c32d6a7cbaa39c7a14a300
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-06-09 02:14:31 +0530

    packing modoules

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,3 +1,13 @@
 """
 intialising routes as a module
 """
+
+import youtube
+from .ask import router as ask
+from .health import router as health
+
+__all__ = [
+    "ask",
+    "health",
+    "youtube",
+]
--- backend/routes/youtube/__init__.py
+++ backend/routes/youtube/__init__.py
@@ -1,3 +1,11 @@
 """
 initalised youtube routes
 """
+
+from .video_info import router as info
+from .video_subs import router as subs
+
+__all__ = [
+    "info",
+    "subs",
+]
