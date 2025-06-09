6400b5b1b375cdf4b087dfe4acf30dd6ad530e99
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-06-09 17:49:12 +0530

    testing

--- backend/webcrawler/html_md.py
+++ backend/webcrawler/html_md.py
@@ -7,3 +7,13 @@ def return_html_md(html: str) -> str:
     soup = BeautifulSoup(html, "html.parser")
     markdowntext = html2text.html2text(soup.body.prettify())
     return markdowntext
+
+
+if __name__ == "__main__":
+    import requests
+
+    res = requests.get("https://portfolio.tashif.codes")
+    html = res.text
+    soup = BeautifulSoup(html, "html.parser")
+    markdown = return_html_md(str(soup))
+    print(markdown)
