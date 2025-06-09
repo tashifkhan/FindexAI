05fc68dbd594034154493ba20f65a6277be20560
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-06-09 18:00:36 +0530

    comparing differnt md conversion methods

--- backend/webcrawler/html_md.py
+++ backend/webcrawler/html_md.py
@@ -12,8 +12,22 @@ def return_html_md(html: str) -> str:
 if __name__ == "__main__":
     import requests
 
-    res = requests.get("https://portfolio.tashif.codes")
+    url = "https://portfolio.tashif.codes"
+
+    # bs4
+    res = requests.get(url)
     html = res.text
     soup = BeautifulSoup(html, "html.parser")
     markdown = return_html_md(str(soup))
     print(markdown)
+
+    # jinja ai
+    from request_md import return_markdown
+
+    __markdown = return_markdown(url)
+    print(__markdown)
+
+    # conparision
+    print("\n\nMarkdown length:", len(markdown))
+    print("Jina AI Markdown length:", len(__markdown))
+    print("Markdown is equal to Jina AI Markdown:", markdown == __markdown)
